1c2752f918f3c918fa05b3e9bcc24bb29d6f931c
Kai Blin <kblin@biosustain.dtu.dk>
2021-10-10 11:48:36 +0200

    repo: Add an option to just import the json for a syntax check
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- src/repo.rs
+++ src/repo.rs
@@ -41,8 +41,13 @@ structopt::clap::arg_enum! {
 
 #[derive(Debug, StructOpt)]
 pub struct RepoImportOpts {
-    #[structopt(parse(from_os_str), help = "File or directory containing MIBiG JSON data")]
+    #[structopt(
+        parse(from_os_str),
+        help = "File or directory containing MIBiG JSON data"
+    )]
     input: std::path::PathBuf,
+    #[structopt(short, long, help = "Only run json parsing")]
+    json_only: bool,
 }
 
 pub fn repo(cfg: RepoOpts) {
@@ -51,7 +56,7 @@ pub fn repo(cfg: RepoOpts) {
     match cfg.cmd {
         Some(cmd) => match cmd {
             RepoSubcommand::List(opts) => repo_list(opts.status),
-            RepoSubcommand::Import(opts) => repo_import(opts.input, conn),
+            RepoSubcommand::Import(opts) => repo_import(opts.input, conn, opts.json_only),
         },
         None => repo_list(RepoListEntryStatus::Published),
     }
@@ -59,11 +64,10 @@ pub fn repo(cfg: RepoOpts) {
 
 use crate::models;
 
-fn repo_import(input: std::path::PathBuf, conn: PgConnection) {
-
+fn repo_import(input: std::path::PathBuf, conn: PgConnection, json_only: bool) {
     if input.is_file() {
-        import_file(input, None, &conn);
-        return
+        import_file(input, None, &conn, json_only);
+        return;
     }
 
     let mut taxon_cache: HashMap<i64, utils::taxa::NcbiTaxEntry> = HashMap::new();
@@ -72,17 +76,35 @@ fn repo_import(input: std::path::PathBuf, conn: PgConnection) {
         let entry = entry_option.unwrap();
         let path = entry.path();
         if "json" != path.extension().unwrap() {
-            continue
+            continue;
         }
         eprintln!("Loading {:?}", path);
-        import_file(path, Some(&mut taxon_cache), &conn);
+        import_file(path, Some(&mut taxon_cache), &conn, json_only);
     }
 }
 
-fn import_file(infile: std::path::PathBuf, cache: Option<&mut HashMap<i64, utils::taxa::NcbiTaxEntry>>, conn: &PgConnection) {
+fn import_file(
+    infile: std::path::PathBuf,
+    cache: Option<&mut HashMap<i64, utils::taxa::NcbiTaxEntry>>,
+    conn: &PgConnection,
+    json_only: bool,
+) {
     let content = std::fs::read_to_string(&infile).expect("could not read file");
     let entry: models::schema::Entry = serde_json::from_str(&content).unwrap();
 
+    if !json_only {
+        insert_into_db(&entry, cache, conn);
+    }
+
+    let return_value = serde_json::to_string_pretty(&entry).expect("failed to serialize");
+    println!("{}", return_value)
+}
+
+fn insert_into_db(
+    entry: &models::schema::Entry,
+    cache: Option<&mut HashMap<i64, utils::taxa::NcbiTaxEntry>>,
+    conn: &PgConnection,
+) {
     let tax_id = get_or_create_taxid(
         entry.cluster.organism_name.as_str(),
         entry.cluster.ncbi_tax_id,
@@ -116,9 +138,7 @@ fn import_file(infile: std::path::PathBuf, cache: Option<&mut HashMap<i64, utils
         .get_result(conn)
         .expect("Error saving entry");
 
-    println!("{:?}", created_entry);
-    let return_value = serde_json::to_string_pretty(&entry).expect("failed to serialize");
-    println!("{}", return_value)
+    eprintln!("{:?}", created_entry);
 }
 
 fn get_or_create_taxid<'a>(
